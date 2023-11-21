cd4a63e00c9a35f76be79620e0642f9932623c86
Paulo Tuppy <130563312+TuppyPower@users.noreply.github.com>
2023-11-21 09:01:17 -0300

    Add files via upload

--- tasks_web/lib/tasks_web/router.ex
+++ tasks_web/lib/tasks_web/router.ex
@@ -1,35 +1,13 @@
 defmodule TasksWeb.Router do
-  use Plug.Router
+  use Goldcrest.Router,
+    parser_options: [
+      parsers: [:urlencoded, :multipart],
+      pass: ["text/html", "application/*"]
+    ]
 
   alias TasksWeb.TaskController
 
-  plug(Plug.Parsers,
-    parsers: [:urlencoded, :multipart],
-    pass: ["text/html", "application/*"]
-  )
-
-  plug(:parse_body)
-  plug(:match)
-  plug(:dispatch)
-
-  get("/tasks/:id/delete", do: TaskController.call(conn, action: :delete))
-  get("/tasks", do: TaskController.call(conn, action: :index))
-  post("/tasks", do: TaskController.call(conn, action: :create))
-
-  match _ do
-    send_resp(conn, 404, "<h1>Not Found</h1>")
-  end
-
-  defp parse_body(conn, _opts) do
-    {:ok, body, conn} = Plug.Conn.read_body(conn, length: 1_000_000)
-
-    body_params = decode_body_params(body)
-    params = Map.merge(conn.params, body_params)
-    conn = %{conn | body_params: body_params, params: params}
-
-    conn
-  end
-
-  defp decode_body_params(""), do: %{}
-  defp decode_body_params(binary), do: Jason.decode!(binary)
+  get("/tasks/:id/delete", TaskController, :delete)
+  get("/tasks", TaskController, :index)
+  post("/tasks", TaskController, :create)
 end
